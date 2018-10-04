22d64c4c43938d44c80ecf355306bbf30d0c3e2c
rkg18 <rkg18@zips.uakron.edu>
2018-10-04 16:22:08 -0400

    added config for sqlite db and initilized 'user' table

--- imprint/__init__.py
+++ imprint/__init__.py
@@ -9,9 +9,19 @@ def create_app(test_config=None):
     # Creates Instance & Tells App where files are relative
     app = Flask(__name__, instance_relative_config=True)
 
+    """ Sets configuration for DB with SQLITE3 """
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'imprint.sqlite')
+    )
+
     # a simple page that says hello
     @app.route('/')
     def hello():
         return '<h1> Hello World </h1>'
+
+    # Adds DB to app
+    from . import db
+    db.init_app(app)
 
     return app
--- imprint/db.py
+++ imprint/db.py
@@ -1,20 +1,47 @@
 import sqlite3
 import click
 
+import pdb
+
 from flask import current_app, g
 from flask.cli import with_appcontext
 
+#pdb.set_trace() # Debug Start
+
+""" Get Database """
 def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(
             current_app.config['DATABASE'],
             detect_types=sqlite3.PARSE_DECLTYPES
         )
+        g.db.row_factory = sqlite3.Row
 
     return g.db
 
+""" Close database """
 def close_db(e=None):
     db = g.pop('db', None)
 
     if db is not None:
         db.close()
+
+""" initizlies database """
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+""" Starts DB """
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
+
+""" Register functions of app instances """
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
