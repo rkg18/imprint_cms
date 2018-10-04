6c27b42baa8e554111c535298a8210648e271513
rkg18 <rkg18@zips.uakron.edu>
2018-10-04 15:37:31 -0400

    removed test config for now

--- imprint/__init__.py
+++ imprint/__init__.py
@@ -3,34 +3,16 @@
 import os
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy # Database connection
 
 """ Creates and Configures Application """
 def create_app(test_config=None):
     # Creates Instance & Tells App where files are relative
     app = Flask(__name__, instance_relative_config=True)
 
-    # Default config settings
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
     # a simple page that says hello
     @app.route('/')
     def hello():
-        return 'Hello, World!'
+        return '<h1> Hello World </h1>'
 
     return app
