b257a772256ead5309259b35fa3df94beab06f35
rkg18 <rkg18@zips.uakron.edu>
2018-10-10 18:06:45 -0400

    changing posts table

--- imprint/blog.py
+++ imprint/blog.py
@@ -6,6 +6,15 @@ from imprint.db import get_db
 
 from imprint.auth import login_required
 
+@bp.route('/blog')
+def blog_index():
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.post_id, title, url, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+
 """ Add Post function lets user add a blog post to their website """
 @bp.route('/add-post', methods=('GET','POST'))
 @login_required
