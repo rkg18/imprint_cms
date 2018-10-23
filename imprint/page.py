7694a8e5caf696be28834a43876b1546523fe95d
rkg18 <rkg18@zips.uakron.edu>
2018-10-23 16:03:48 -0400

    added flask code for add_page

--- imprint/page.py
+++ imprint/page.py
@@ -10,7 +10,29 @@ bp = Blueprint('Page', __name__)
 @bp.route('/landing-page', methods=('GET','POST'))
 def landing_page():
     if request.method == 'POST':
-        pass
+        page_type = request.form.get('page-types')
+
+        heading = request.form['heading']
+        subheading = request.form['subheading']
+        button_text = request.form['button-text']
+
+        error = None
+
+        if not heading:
+            error = "A 'Heading' is required"
+        elif not subheading:
+            error = "A 'Subheading' is required"
+        elif not button_text:
+            error = "Button Text is required"
+
+        if error is not None:
+            flash(error)
+        else:
+            # db = get_db()
+            # db.execute("INSERT INTO posts (title, body, url) VALUES (?,?,?)",(title,body,url))
+            # db.commit()
+
+            # return redirect(url_for('blog.blog_index'))
 
 @bp.route('/product-page')
 def product_page():
