7ab4a056e8811f01e7464a10dd9deac0b1e86507
rkg18 <rkg18@zips.uakron.edu>
2018-10-23 15:51:17 -0400

    added add_page.html

--- imprint/page.py
+++ imprint/page.py
@@ -7,6 +7,11 @@ from imprint.auth import login_required
 
 bp = Blueprint('Page', __name__)
 
-@bp.route('landing-page')
+@bp.route('/landing-page', methods=('GET','POST'))
 def landing_page():
+    if request.method == 'POST':
+        pass
+
+@bp.route('/product-page')
+def product_page():
     pass
