379bcefdbbfd983bcbb90aa329c9aba39ad1a7bd
rkg18 <rkg18@zips.uakron.edu>
2018-10-31 20:59:58 -0400

    TODO: Finish the settings

--- imprint/static/script.js
+++ imprint/static/script.js
@@ -10,3 +10,14 @@ function checkPageType(that) {
     document.getElementById("product-page-display").style.display = "block";
   }
 }
+
+function changeFontSize(that) {
+  if (that.value == 16)
+  {
+    document.getElementById("base").style.fontSize = "16px";
+  }
+  else if(that.value == 18)
+  {
+    document.getElementById("base").style.fontSize = "18px";
+  }
+}
