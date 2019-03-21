bf993028327ccfd042f40933c3c0972e756af39e
rkg18 <rkg18@zips.uakron.edu>
2019-03-20 20:57:20 -0400

    Add stylings for text change when switching between dark and light theme

--- imprint/__init__.py
+++ imprint/__init__.py
@@ -66,8 +66,4 @@ def create_app(test_config=None):
     from . import address
     app.register_blueprint(address.bp)
 
-    """ Live Site """
-    from . import live
-    app.register_blueprint(live.bp)
-
     return app
